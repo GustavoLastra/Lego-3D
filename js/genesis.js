2d38a9729fc17da24e73427c361501a936a5d4e7
Gustavo Lastra <gustavo.lastra@bee.io>
2017-07-03 13:55:14 +0200

    Street is there

---
+++ js/genesis.js
@@ -0,0 +1,54 @@
+
+//Vertical streets
+$('scene').append('<Transform id="vs1" translation="-2 3 0" rotation="0 0 1 1.5708" />');
+$('#vs1').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs2" translation="-2 1 0" rotation="0 0 1 1.5708" />');
+$('#vs2').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs3" translation="-2 0 0" rotation="0 0 1 1.5708" />');
+$('#vs3').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs4" translation="-2 -1 0" rotation="0 0 1 1.5708" />');
+$('#vs4').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs5" translation="-2 -3 0" rotation="0 0 1 1.5708" />');
+$('#vs5').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs6" translation="2 3 0" rotation="0 0 1 1.5708" />');
+$('#vs6').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs7" translation="2 1 0" rotation="0 0 1 1.5708" />');
+$('#vs7').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs8" translation="2 0 0" rotation="0 0 1 1.5708" />');
+$('#vs8').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs9" translation="2 -1 0" rotation="0 0 1 1.5708" />');
+$('#vs9').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="vs10" translation="2 -3 0" rotation="0 0 1 1.5708" />');
+$('#vs10').append('<Inline url="x3d/street.x3d" />');
+
+//Horizontal streets
+$('scene').append('<Transform id="hs1" translation="-1 2 0" />');
+$('#hs1').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs2" translation="0 2 0" />');
+$('#hs2').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs3" translation="1 2 0" />');
+$('#hs3').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs4" translation="-1 -2 0" />');
+$('#hs4').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs5" translation="0 -2 0" />');
+$('#hs5').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs6" translation="1 -2 0" />');
+$('#hs6').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs7" translation="-3 2 0" />');
+$('#hs7').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs8" translation="3 2 0" />');
+$('#hs8').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs9" translation="3 -2 0" />');
+$('#hs9').append('<Inline url="x3d/street.x3d" />');
+$('scene').append('<Transform id="hs10" translation="-3 -2 0" />');
+$('#hs10').append('<Inline url="x3d/street.x3d" />');
+
+//crossroads
+$('scene').append('<Transform id="cross1" translation="-2 2 0" />');
+$('#cross1').append('<Inline url="x3d/crossroad.x3d" />');
+$('scene').append('<Transform id="cross2" translation="2 2 0" />');
+$('#cross2').append('<Inline url="x3d/crossroad.x3d" />');
+$('scene').append('<Transform id="cross3" translation="-2 -2 0" />');
+$('#cross3').append('<Inline url="x3d/crossroad.x3d" />');
+$('scene').append('<Transform id="cross4" translation="2 -2 0" />');
+$('#cross4').append('<Inline url="x3d/crossroad.x3d" />');
